4d268d136c782e7ab4199c05bc0e94376259a25a
Jazmin Amato <proyectos.amato@gmail.com>
2024-06-10 02:11:48 -0300

    update app

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const moviesRouter = require('./routes/movies')
 
 app.use(express.json())
 
+app.get('/', (req,res) => res.send("Welcome to Movies"))
+
 app.use('/movies',moviesRouter)
 
-const PORT = 3000
-app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`)
-})
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`))
